9e18efcbf44927c69b895f26708df30fc3f4f90e
TOLSTIY <kandreev1404@gmail.com>
2023-12-18 17:10:52 +0700

    clear version

--- async_practic.py
+++ async_practic.py
@@ -13,23 +13,21 @@ async def page_parce(session, page): # работа одной задачи
 
     url = f"https://com-x.life/comix-read/marvel-read/page/{str(page)}"
     async with session.get(url=url, headers=headers) as response:
-        pass
-    # StoryName = []
-    # async with aiohttp.ClientSession() as session:
-    #     response = await session.get(url=url, headers=headers)
-    #     soup = BeautifulSoup(await response.text(), 'lxml')
-    # names = soup.find("div", class_="content-masonry").find_all("article")
 
-    # for name in names:
-    #     try:
-    #         name = name.find("div", class_="article-inf").find("a", class_="title").text
-    #         StoryName.append(f"{name.strip()}_{page}")
-    #     except:
-    #         continue
+        StoryName = []
+
+        soup = BeautifulSoup(await response.text(), 'lxml')
+        names = soup.find("div", class_="content-masonry").find_all("article")
+
+        for name in names:
+            try:
+                name = name.find("div", class_="article-inf").find("a", class_="title").text
+                StoryName.append(f"{name.strip()}_{page}")
+            except:
+                continue
 
     print("[INFO] ONE MORE PAGE SUCCESS")
 
-    pass
 
 async def work_gather(): # список задач
     tasks = []
@@ -39,13 +37,13 @@ async def work_gather(): # список задач
         soup = BeautifulSoup(await response.text(), 'lxml')
         i = soup.text
     pag_block = soup.find("div", class_="wrapper")
-    # PageEnd = pag_block[2].text
+    PageEnd = pag_block[2].text
 
-    # for page in range(1, int(PageEnd)+1):
-    #     task = asyncio.create_task(page_parce(session, page))
-    #     tasks.append(task)
+    for page in range(1, int(PageEnd)+1):
+        task = asyncio.create_task(page_parce(session, page))
+        tasks.append(task)
 
-    # await asyncio.gather(*tasks)
+    await asyncio.gather(*tasks)
 
     pass
 
